9cc44036867ba86e6c21b31a60099c42ae8e451b
vincentr21 <vincent.ren21@gmail.com>
2016-07-30 18:55:57 -0400

    added basic agent with state info

--- bidding_game.py
+++ bidding_game.py
@@ -2,8 +2,17 @@
 
 import logging, sys
 
+# start of turn game state variables
+
 my_bank = []
 enemy_bank = []
+
+my_nominal = []
+enemy_nominal =[]
+
+my_steps_to_win = []
+enemy_steps_to_win = []
+
 turn = -1
 draw_adv = []
 
@@ -17,32 +26,77 @@ def calculate_bid(player,pos,first_moves,second_moves):
         my_moves = second_moves
         enemy_moves = first_moves
     
-    # update game state info
+    # game state variables
     global turn
-    global first_bank
-    global second_bank
+    global my_bank
+    global enemy_bank
+    global my_nominal
+    global enemy_nominal
+    global my_steps_to_win
+    global enemy_steps_to_win 
+    global turn
+    
+    #================================= update game state variables ===============================================
     
     # turn is 0-indexed
-    turn += 1
+    turn = turn + 1
+    my_bank.append(123)
     
-    # initial state
+    # first turn initial state
     if turn == 0:
         my_bank.append(100)
         enemy_bank.append(100)
-        draw_adv.append[1]
+        
+        my_nominal.append(10)
+        enemy_nominal.append(10)
+        
+        my_steps_to_win.append(5)
+        enemy_steps_to_win.append(5)
+        
+        draw_adv.append(1)
+        
+    # mid game states
     else:
-        if my_moves[turn-1] 
+        # case: same bid amount
+        if my_moves[turn-1] == enemy_moves[turn-1]:
+            if player == 1:
+                if draw_adv[turn-1] == 1:
+                    my_bank.append(my_bank[turn-1] - my_move[turn-1])
+                else:
+                    enemy_bank.append(enemy_bank[turn-1] - enemy_move[turn-1])
+            else: #player is 2
+                if draw_adv[turn-1] == 1:
+                    enemy_bank.append(enemy_bank[turn-1] - enemy_move[turn-1])
+                else:
+                    my_bank.append(my_bank[turn-1] - my_move[turn-1])
+            draw_adv.append(3-draw_adv[turn-1])
+        # case: my bid higher    
+        elif my_moves[turn-1] > enemy_moves[turn-1]:
+            my_bank.append(my_bank[turn-1] - my_move[turn-1])
+        # case: enemy bid higher
+        else:
+            enemy_bank.append(enemy_bank[turn-1] - enemy_move[turn-1])
+            
+        my_steps_to_win.append(pos)
+        enemy_steps_to_win.append(10-pos)
+        
+        my_nominal.append(my_bank[turn]/(my_steps_to_win[turn]*2))
+        enemy_nominal.append(enemy_bank[turn]/(enemy_steps_to_win[turn]*2))
     
+    # now for the strategies
+    my_bid = my_nominal[turn]
     
+    debugMsg = 'turn: ' + str(turn) + '\n'
+    debugMsg += 'my moves: ' + str(my_moves) + '\n'
+    debugMsg += 'enemy moves: ' + str(enemy_moves) + '\n'
+    debugMsg += 'my bank: ' + str(my_bank) + '\n'
+    debugMsg += 'enemy bank: ' + str(enemy_bank) + '\n'
+    debugMsg += 'my nominal: ' + str(my_nominal) + '\n'
+    debugMsg += 'enemy nominal: ' + str(enemy_nominal) + '\n'
     
-    
-    debugMsg = 'turn: ' + str(len(first_moves)) + '\n'
-    debugMsg += str(first_moves) + '\n'
-    
-    """your logic here"""
     sys.stderr.write(debugMsg)
     
-    return 5
+    return my_bid
 
 
 
