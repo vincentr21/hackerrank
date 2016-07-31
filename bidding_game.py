c25aa0443234722ecd93cb473a0a01e5d9f77905
vincentr21 <vincent.ren21@gmail.com>
2016-07-31 14:00:52 -0400

    v1 create

--- bidding_game.py
+++ bidding_game.py
@@ -2,21 +2,10 @@
 
 import logging, sys
 
-# start of turn game state variables
-
-my_bank = []
-enemy_bank = []
-
-my_nominal = []
-enemy_nominal =[]
-
-my_steps_to_win = []
-enemy_steps_to_win = []
-
-turn = -1
-draw_adv = []
-
 def calculate_bid(player,pos,first_moves,second_moves):
+        
+    # turn is 0-indexed
+    turn = len(first_moves)
     
     # helper function
     if player == 1:
@@ -27,64 +16,78 @@ def calculate_bid(player,pos,first_moves,second_moves):
         enemy_moves = first_moves
     
     # game state variables
-    global turn
-    global my_bank
-    global enemy_bank
-    global my_nominal
-    global enemy_nominal
-    global my_steps_to_win
-    global enemy_steps_to_win 
-    global turn
-    
+    my_bank = []
+    enemy_bank = []
+
+    my_nominal = []
+    enemy_nominal =[]
+
+    my_steps_to_win = []
+    enemy_steps_to_win = []
+
+    draw_adv = []
     #================================= update game state variables ===============================================
-    
-    # turn is 0-indexed
-    turn = turn + 1
-    my_bank.append(123)
-    
+
     # first turn initial state
-    if turn == 0:
-        my_bank.append(100)
-        enemy_bank.append(100)
-        
-        my_nominal.append(10)
-        enemy_nominal.append(10)
-        
-        my_steps_to_win.append(5)
-        enemy_steps_to_win.append(5)
-        
-        draw_adv.append(1)
+    my_bank.append(100)
+    enemy_bank.append(100)
+
+    my_nominal.append(10)
+    enemy_nominal.append(10)
+
+    my_steps_to_win.append(5)
+    enemy_steps_to_win.append(5)
+
+    draw_adv.append(1)
         
     # mid game states
-    else:
+    for T in range(1,turn+1):
         # case: same bid amount
-        if my_moves[turn-1] == enemy_moves[turn-1]:
+        if my_moves[T-1] == enemy_moves[T-1]:
             if player == 1:
-                if draw_adv[turn-1] == 1:
-                    my_bank.append(my_bank[turn-1] - my_move[turn-1])
+                if draw_adv[T-1] == 1:
+                    my_bank.append(my_bank[T-1] - my_moves[T-1])
+                    enemy_bank.append(enemy_bank[T-1])
+                    my_steps_to_win.append(my_steps_to_win[T-1]-1)
+                    enemy_steps_to_win.append(enemy_steps_to_win[T-1]+1)
                 else:
-                    enemy_bank.append(enemy_bank[turn-1] - enemy_move[turn-1])
+                    my_bank.append(my_bank[T-1])
+                    enemy_bank.append(enemy_bank[T-1] - enemy_moves[T-1])
+                    my_steps_to_win.append(my_steps_to_win[T-1]+1)
+                    enemy_steps_to_win.append(enemy_steps_to_win[T-1]-1)
             else: #player is 2
-                if draw_adv[turn-1] == 1:
-                    enemy_bank.append(enemy_bank[turn-1] - enemy_move[turn-1])
+                if draw_adv[T-1] == 1:
+                    my_bank.append(my_bank[T-1])
+                    enemy_bank.append(enemy_bank[T-1] - enemy_moves[T-1])
+                    my_steps_to_win.append(my_steps_to_win[T-1]+1)
+                    enemy_steps_to_win.append(enemy_steps_to_win[T-1]-1)
                 else:
-                    my_bank.append(my_bank[turn-1] - my_move[turn-1])
-            draw_adv.append(3-draw_adv[turn-1])
+                    my_bank.append(my_bank[T-1] - my_moves[T-1])
+                    enemy_bank.append(enemy_bank[T-1])
+                    my_steps_to_win.append(my_steps_to_win[T-1]-1)
+                    enemy_steps_to_win.append(enemy_steps_to_win[T-1]+1)
+            draw_adv.append(3-draw_adv[T-1])
         # case: my bid higher    
-        elif my_moves[turn-1] > enemy_moves[turn-1]:
-            my_bank.append(my_bank[turn-1] - my_move[turn-1])
+        elif my_moves[T-1] > enemy_moves[T-1]:
+            my_bank.append(my_bank[T-1] - my_moves[T-1])
+            enemy_bank.append(enemy_bank[T-1])
+            my_steps_to_win.append(my_steps_to_win[T-1]-1)
+            enemy_steps_to_win.append(enemy_steps_to_win[T-1]+1)
         # case: enemy bid higher
         else:
-            enemy_bank.append(enemy_bank[turn-1] - enemy_move[turn-1])
-            
-        my_steps_to_win.append(pos)
-        enemy_steps_to_win.append(10-pos)
+            my_bank.append(my_bank[T-1])
+            enemy_bank.append(enemy_bank[T-1] - enemy_moves[T-1])
+            my_steps_to_win.append(my_steps_to_win[T-1]+1)
+            enemy_steps_to_win.append(enemy_steps_to_win[T-1]-1)
+
         
-        my_nominal.append(my_bank[turn]/(my_steps_to_win[turn]*2))
-        enemy_nominal.append(enemy_bank[turn]/(enemy_steps_to_win[turn]*2))
+        my_nominal.append(my_bank[T]/(my_steps_to_win[T]*1))
+        enemy_nominal.append(enemy_bank[T]/(enemy_steps_to_win[T]*1))
+    
     
     # now for the strategies
     my_bid = my_nominal[turn]
+    #my_bid = 10
     
     debugMsg = 'turn: ' + str(turn) + '\n'
     debugMsg += 'my moves: ' + str(my_moves) + '\n'
